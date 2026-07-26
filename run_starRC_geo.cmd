960f1c1fb6f2761f4e43e0c42261ec9364cf601f
edaworker <edaworker@example.com>
2026-07-27 02:29:11 +0800

    add reduced + geo SPEFs for global-routed log2 (top)

---
+++ run_starRC_geo.cmd
@@ -0,0 +1,24 @@
+BLOCK: top
+
+LEF_FILE: /home/edaworker/asap7nm/asap7sc7p5t_28/techlef_misc/asap7_tech_1x_201209.lef
+LEF_FILE: /home/edaworker/asap7nm/asap7sc7p5t_28/LEF/asap7sc7p5t_28_R_1x_220121a.lef
+
+TOP_DEF_FILE: /home/edaworker/spef_w_layer/log2/top_rvt_gr.def
+TCAD_GRD_FILE: /home/edaworker/ASAP7-Synopsys-Enablement/ASAP7/tlup/asap7_1x.nxtgrd
+MAPPING_FILE: /home/edaworker/ASAP7-Synopsys-Enablement/ASAP7/tlup/asap7.layermap
+
+NETS: *
+EXTRACTION: RC
+OPERATING_TEMPERATURE: 25
+DPT: NO
+NUM_CORES: 8
+DENSITY_BASED_THICKNESS: YES
+COUPLE_TO_GROUND: YES
+
+REDUCTION: NO
+NETLIST_TAIL_COMMENTS: YES
+EXTRA_GEOMETRY_INFO: NODE
+
+NETLIST_FORMAT: SPEF
+NETLIST_FILE: /home/edaworker/spef_w_layer/log2/log2_top_rvt_gr_layer.spef
+STAR_DIRECTORY: /home/edaworker/spef_w_layer/log2/starrc_geo
